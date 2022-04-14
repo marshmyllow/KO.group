1c70605d7877c8a68ea0a3a343d61aafa901e3d0
marshmyllow <59705570+marshmyllow@users.noreply.github.com>
2022-04-14 20:40:22 +0300

    доработка меню в мобильной версии

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 (function(){
 	const burderItem = document.querySelector('.burger');
 	const menu = document.querySelector('.header_nav');
-	const menuCloseItem = document.querySelector('.header_nav-close')
+	const menuCloseItem = document.querySelector('.header_nav-close');
+	const menuLinks = document.querySelectorAll('.header_item')
 	burderItem.addEventListener('click', () => {
 		menu.classList.add('header_nav_active');
 	});
 	menuCloseItem.addEventListener('click', () =>{
 		menu.classList.remove('header_nav_active');
 	});
+	if (window.innerWidth <= 850) {
+		for (let i = 0; i < menuLinks.length; i += 1) {
+			menuLinks[i].addEventListener('click', () => {
+				menu.classList.remove('header_nav_active');
+			});
+		}
+	}
 }());
