80ba8be923cc3c238bf736d85f3f0c3bf153d7be
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-05-10 22:42:30 +0300

    форма в тг

---
+++ telegram-lesson/telegram.php
@@ -0,0 +1,29 @@
+<?php
+
+/* https://api.telegram.org/bot5302953493:AAG-jNha14NTXjLEe4eu7VkDQdABYl9lWVY/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$communication = $_POST['communication_type'];
+$token = "5302953493:AAG-jNha14NTXjLEe4eu7VkDQdABYl9lWVY";
+$chat_id = "-772644912";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
