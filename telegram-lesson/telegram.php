b3c4fee7c2f657dbe782cda020754f72aa02f3d3
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-07-10 20:19:33 +0300

    1

--- telegram-lesson/telegram.php
+++ telegram-lesson/telegram.php
@@ -21,9 +21,9 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-// if ($sendToTelegram) {
-//   header('Location: thank-you.html');
-// } else {
-//   echo "Error";
-// }
+if ($sendToTelegram) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
 ?>
